d06c64b925144df7de148691d0e7fc41b60f7765
Brody Much <bamuch@students.ntc.edu>
2023-10-23 10:36:11 -0500

    121 User is able to add a flashcard from flashcards page to a deck of their choice

--- NtcMaui/NtcMaui/Views/MyStudies/AddFlashcardToDeckPage.xaml.cs
+++ NtcMaui/NtcMaui/Views/MyStudies/AddFlashcardToDeckPage.xaml.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Text;
 using System.Text.Json;
 using ApiStudyBuddy.Models;
 
@@ -25,7 +26,6 @@ public partial class AddFlashcardToDeckPage : ContentPage, IQueryAttributable, I
     {
         base.OnAppearing();
         CurrentFlashCardLabel.Text = $"Current Flashcard you wish to add: {SelectedFlashCard.FlashCardQuestion}";
-        UserDecks = await GetAllDecks();
         DeckPicker.ItemsSource = await GetAllDecks();
     }
 
@@ -50,6 +50,113 @@ public partial class AddFlashcardToDeckPage : ContentPage, IQueryAttributable, I
         return decks;
     }
 
+    private async void AddFlashcardToDeckBtn_Clicked(object sender, EventArgs e)
+    {
+        bool failed = false;
+        ErrorLabel.IsVisible = false;
+        Deck selectedDeck = await GetDeckByDeckName(DeckName);
+        if (selectedDeck == null || selectedDeck.DeckId == 0)
+        {
+            ErrorLabel.Text = "Please Select a deck from the drop-down";
+            ErrorLabel.IsVisible = true;
+        }
+        else
+        {
+            //check to see if the flashcard already exists and if it does shoot an error, else continue like normal
+            foreach(DeckFlashCard flashcard in selectedDeck.DeckFlashCards)
+            {
+                if (flashcard.FlashCardId == SelectedFlashCard.FlashCardId)
+                {
+                    failed = true;
+                    break;
+                }
+            }
+            //then add the Selected flashcard to DeckFlashcards Post.
+            if (failed)
+            {
+                ErrorLabel.Text = "Flashcard you are trying to add already exists in the deck. Please choose another deck or choose a different flashcard";
+                ErrorLabel.IsVisible = true;
+            }
+            else
+            {
+                DeckFlashCard deckFlashCard = new DeckFlashCard();
+                deckFlashCard.FlashCardId = SelectedFlashCard.FlashCardId;
+                deckFlashCard.DeckId = selectedDeck.DeckId;
+                await SaveDeckFlashCardAsync(deckFlashCard);
+
+                //then go back to the flashcards page
+                var navigationParameter = new Dictionary<string, object>
+                {
+                    { "Current User", LoggedInUser }
+                };
+                await Shell.Current.GoToAsync(nameof(FlashcardPage), navigationParameter);
+            }
+        }
+    }
+
+    //gets the Deckname when a user chooses an option
+    private void DeckPicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+        if (selectedIndex != -1)
+        {
+            //use this deckname to eventually find a deck in the next btn.
+            ErrorLabel.IsVisible = false;
+            DeckName = picker.Items[selectedIndex];
+        }
+    }
+
+    /// <summary>
+    /// gets the deck based on deck name
+    /// </summary>
+    /// <returns>a deck</returns>
+    public async Task<Deck> GetDeckByDeckName(string DeckName)
+    {
+        Deck deck = new Deck();
+
+        Uri uri = new Uri(string.Format($"{Constants.TestUrl}/api/Deck/deckname/{DeckName}", string.Empty));
+        try
+        {
+            HttpResponseMessage response = await Constants._client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                deck = JsonSerializer.Deserialize<Deck>(content, Constants._serializerOptions);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(@"\tERROR {0}", ex.Message);
+        }
+
+        return deck;
+    }
+
+    //posts the DeckFlashcard
+    public async Task SaveDeckFlashCardAsync(DeckFlashCard deckFlashCard)
+    {
+        Uri uri = new Uri(string.Format($"{Constants.TestUrl}/api/DeckFlashCard", string.Empty));
+
+        try
+        {
+            string json = JsonSerializer.Serialize<DeckFlashCard>(deckFlashCard, Constants._serializerOptions);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+            response = await Constants._client.PostAsync(uri, content);
+
+            if (response.IsSuccessStatusCode)
+                Debug.WriteLine(@"\tdeckFlashCard successfully saved.");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(@"\tERROR {0}", ex.Message);
+        }
+    }
+
+    public string DeckName { get; set; }
+
     public User LoggedInUser { get; set; }
 
     public List<UserDeck> UserDecks { get; set; }
@@ -57,21 +164,4 @@ public partial class AddFlashcardToDeckPage : ContentPage, IQueryAttributable, I
     public UserDeck SelectedUserDeck { get; set; }
 
     public FlashCard SelectedFlashCard { get; set; }
-
-
-
-    private void AddFlashcardToDeckBtn_Clicked(object sender, EventArgs e)
-    {
-    }
-
-    private void DeckPicker_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        var picker = (Picker)sender;
-        int selectedIndex = picker.SelectedIndex;
-        if (selectedIndex != -1)
-        {
-            //if this is stuck as a string I can sit there and maybe make a endpoint to find the deck as based on the string
-           var test = picker.Items[selectedIndex];
-        }
-    }
 }
